21111d477feb6ba72f3dda9ef3e68c62f0e6061d
Dominykas Kutka <dominykaskulka@yahoo.com>
2020-05-19 16:37:14 +0200

    adds footer template

---
+++ src/components/Footer/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//Styles
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div id="Footer">
+      <div></div>
+    </div>
+  );
+};
+
+export default Footer;
