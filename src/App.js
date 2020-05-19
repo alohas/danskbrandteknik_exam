b31f227b90162eff25910efda1ff34510daa1f31
Dominykas Kutka <dominykaskulka@yahoo.com>
2020-05-19 16:46:55 +0200

    Sets up layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 
+//Components
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+
 //Styles
 import "./styles/App.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div id="App">
+      <NavBar />
+
       <div></div>
+
+      <Footer />
     </div>
   );
 }
