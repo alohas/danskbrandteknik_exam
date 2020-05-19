f82151e12662884b4a6debe16d4d369aa895623f
Dominykas Kutka <dominykaskulka@yahoo.com>
2020-05-19 14:30:58 +0200

    sets up react and sass boiler plate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//Styles
+import "./styles/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
