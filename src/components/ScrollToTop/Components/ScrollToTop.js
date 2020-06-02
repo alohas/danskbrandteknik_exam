62c42502f30693f62eb65fa19014fe17a92b8e85
Dominykas Kutka <dominykaskulka@yahoo.com>
2020-06-02 21:02:03 +0200

    adds hook to hide scroll to top

--- src/components/ScrollToTop/Components/ScrollToTop.js
+++ src/components/ScrollToTop/Components/ScrollToTop.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
+import { useScrollPosition } from "@n8tb1t/use-scroll-position";
 
 //Assets
 import UpSvg from "../../../assets/svg/Up.svg";
@@ -8,9 +9,21 @@ import UpSvg from "../../../assets/svg/Up.svg";
 import "./ScrollToTop.scss";
 
 function ScrollToTop() {
+  let classNames = require("classnames");
+
+  const [hideElement, setHideElement] = useState(true);
+
+  useScrollPosition(({ currPos }) => {
+    if (currPos.y < -800) {
+      setHideElement(false);
+    } else {
+      setHideElement(true);
+    }
+  });
+
   return (
     <Link id="ToTop" to="root" smooth={true}>
-      <UpSvg className="icon" />
+      <UpSvg className={classNames("icon", { hide: hideElement })} />
     </Link>
   );
 }
